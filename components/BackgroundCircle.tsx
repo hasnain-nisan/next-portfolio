920a41b794a02f23ef4ba6d569516f519a61db68
Hasnain Ahamed Nisan <hasnain.nisan@outlook.com>
2022-10-31 18:04:59 +0600

    Modification in background circle component

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -5,30 +5,26 @@ type Props = {}
 
 const BackgroundCircle = (props: Props) => {
 
-  setTimeout(() => {
-    document.getElementById('lastCircle').style.display = 'none';
-  }, 2800);
-
   return (
-    <motion.div 
-        className="relative flex justify-center items-center"
-        initial={{ 
-            opacity: 0
-        }}
-        animate={{ 
-            scale: [1,2,2,3,1],
-            opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
-            borderRadius: ["20%", "20%", "50%", "80%", "20%"]
-        }}
-        transition={{ 
-            duration: 2.5
-        }}
+    <motion.div
+      className="relative flex justify-center items-center"
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+      }}
+      transition={{
+        duration: 2.5,
+      }}
     >
-        <div className="absolute border border-[#33333A] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-        <div className="absolute border border-[#33333A] rounded-full h-[300px] w-[300px] mt-52"/>
-        <div className="absolute border border-[#33333A] rounded-full h-[500px] w-[500px] mt-52"/>
-        <div className="absolute border border-[#F7AB0A] rounded-full h-[650px] w-[650px] mt-52 animate-pulse opacity-20" />
-        <div id='lastCircle' className="absolute border border-[#33333A] rounded-full h-[800px] w-[800px] mt-52"/>
+      <div className="absolute border border-[#33333A] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-[#33333A] rounded-full h-[300px] w-[300px] mt-52" />
+      <div className="absolute border border-[#33333A] rounded-full h-[500px] w-[500px] mt-52" />
+      <div className="absolute border border-[#F7AB0A] rounded-full h-[650px] w-[650px] mt-52 animate-pulse opacity-20" />
+      <div className="absolute border border-[#33333A] rounded-full h-[800px] w-[800px] mt-52 animate-ping"/>
     </motion.div>
   );
 }
